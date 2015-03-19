23b71ac75d420912193445bad8c3498a7e6366db
PtitBlond <bruno.buiret@gmail.com>
2015-03-19 09:24:44 +0100

    Documentation, carte et jeu

--- Projet-AP2/Card.cs
+++ Projet-AP2/Card.cs
@@ -8,6 +8,29 @@ namespace Projet_AP2
 {
     public class Card
     {
+        /// <summary>
+        /// Represents the number on a card.
+        /// </summary>
+        protected Byte number;
 
+        /// <summary>
+        /// Accessor for a card's number.
+        /// </summary>
+        public Byte Number
+        {
+            get 
+            {
+                return this.number;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new card.
+        /// </summary>
+        /// <param name="number">Number on the card.</param>
+        public Card(Byte number)
+        {
+            this.number = number;
+        }
     }
 }
--- Projet-AP2/StupidVulture.cs
+++ Projet-AP2/StupidVulture.cs
@@ -8,6 +8,23 @@ namespace Projet_AP2
 {
     public class StupidVulture
     {
+        /// <summary>
+        /// Represents the list of currently playing players.
+        /// </summary>
+        protected List<Player> players;
 
+        /// <summary>
+        /// Represents the deck of card which are hidden.
+        /// </summary>
+        protected Stack<Card> deck;
+
+        /// <summary>
+        /// Creates a new game of Stupid Vulture.
+        /// </summary>
+        /// <param name="players">List of players for the game.</param>
+        public StupidVulture(List<Player> players)
+        {
+            this.players = players;
+        }
     }
 }
