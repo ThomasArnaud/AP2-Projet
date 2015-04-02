f29857a83607ca4e9419eb42f5c77eeee927ed59
PtitBlond <bruno.buiret@gmail.com>
2015-04-02 08:48:31 +0200

    Fenêtre de statistiques

---
+++ Projet-AP2/Statistics.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Projet_AP2
+{
+    partial class Statistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.playedGamesLabel = new System.Windows.Forms.Label();
+            this.wonGamesLabel = new System.Windows.Forms.Label();
+            this.victoryPercentageLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.closeButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.playedGamesLabel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.wonGamesLabel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.victoryPercentageLabel, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(360, 138);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Parties jouées";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Parties gagnées";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Taux de victoire";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.closeButton.Location = new System.Drawing.Point(282, 112);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Quitter";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // playedGamesLabel
+            // 
+            this.playedGamesLabel.AutoSize = true;
+            this.playedGamesLabel.Location = new System.Drawing.Point(92, 0);
+            this.playedGamesLabel.Name = "playedGamesLabel";
+            this.playedGamesLabel.Size = new System.Drawing.Size(0, 13);
+            this.playedGamesLabel.TabIndex = 4;
+            // 
+            // wonGamesLabel
+            // 
+            this.wonGamesLabel.AutoSize = true;
+            this.wonGamesLabel.Location = new System.Drawing.Point(92, 29);
+            this.wonGamesLabel.Name = "wonGamesLabel";
+            this.wonGamesLabel.Size = new System.Drawing.Size(0, 13);
+            this.wonGamesLabel.TabIndex = 5;
+            // 
+            // victoryPercentageLabel
+            // 
+            this.victoryPercentageLabel.AutoSize = true;
+            this.victoryPercentageLabel.Location = new System.Drawing.Point(92, 61);
+            this.victoryPercentageLabel.Name = "victoryPercentageLabel";
+            this.victoryPercentageLabel.Size = new System.Drawing.Size(0, 13);
+            this.victoryPercentageLabel.TabIndex = 6;
+            // 
+            // Statistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.closeButton;
+            this.ClientSize = new System.Drawing.Size(384, 162);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Statistics";
+            this.Text = "Statistics";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label playedGamesLabel;
+        private System.Windows.Forms.Label wonGamesLabel;
+        private System.Windows.Forms.Label victoryPercentageLabel;
+    }
+}
