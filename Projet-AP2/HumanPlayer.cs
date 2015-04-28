c2cb30e2901f02b526b406930372cfde54c6715f
PtitBlond <bruno.buiret@gmail.com>
2015-04-28 21:31:28 +0200

    COMMENTAIRES !²

--- Projet-AP2/HumanPlayer.cs
+++ Projet-AP2/HumanPlayer.cs
@@ -9,10 +9,19 @@ namespace Projet_AP2
 {
     public class HumanPlayer : Player
     {
+        /// <summary>
+        /// Creates a new human player.
+        /// </summary>
+        /// <param name="number">Player's number.</param>
+        /// <param name="name">Player's name.</param>
+        /// <param name="color">Player's color.</param>
         public HumanPlayer(Byte number, String name, Color color) : base(number, name, color)
         {
         }
 
+        /// <summary>
+        /// This method shouldn't be called because it is handled by the main window.
+        /// </summary>
         public override Byte Play(SByte deckCard)
         {
             throw new NotImplementedException("This method shouldn't be called.");
